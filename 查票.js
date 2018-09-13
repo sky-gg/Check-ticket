332bf595414084c29d8ab41a380d10a469c58424
sky-gg <33249562+sky-gg@users.noreply.github.com>
2018-09-13 16:10:34 +0800

    Update 查票.js

--- 查票.js
+++ 查票.js
@@ -1,12 +1,12 @@
 function checkTicket() {
-  var count = 0;//用于统计查票次数
+  this.count = 0;//用于统计查票次数
   this.timer = setInterval(() => {//每3秒钟查询一次
-    count++;//查询次数加一
+    this.count++;//查询次数加一
     this.a = $("#ZY_5l0000G10470").attr("class") || $("#YZ_58000K13160C").attr("class")|| $("#YW_58000K13160C").attr("class");;
     //$("#ZY_5l0000G10470").attr("class") 有票的座位会包含class名 以此来判断
     // 可以查询多个
     if (this.a) {//存在即有票
-      console.log("共查询：" + count + "次！");//输出查询次数
+      console.log("共查询：" + this.count + "次！");//输出查询次数
       clearInterval(this.timer);//清空计时器 停止查询
       alert("有票，快买");//alert 方法能够在浏览器最小化时自动最大化浏览器
     }
