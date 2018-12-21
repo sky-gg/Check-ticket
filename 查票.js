cec88308dfa77897686b32bf81ef459acf7bbf23
sky-gg <33249562+sky-gg@users.noreply.github.com>
2018-12-21 18:16:59 +0800

    Update 查票.js

--- 查票.js
+++ 查票.js
@@ -1,16 +1,20 @@
-function checkTicket() {
-  this.count = 0;//用于统计查票次数
-  this.timer = setInterval(() => {//每3秒钟查询一次
-    this.count++;//查询次数加一
-    this.a = $("#ZY_5l0000G10470").attr("class") || $("#YZ_58000K13160C").attr("class")|| $("#YW_58000K13160C").attr("class");;
-    //$("#ZY_5l0000G10470").attr("class") 有票的座位会包含class名 以此来判断
-    // 可以查询多个
-    if (this.a) {//存在即有票
-      console.log("共查询：" + this.count + "次！");//输出查询次数
-      clearInterval(this.timer);//清空计时器 停止查询
-      alert("有票，快买");//alert 方法能够在浏览器最小化时自动最大化浏览器
-    }
-    $("#query_ticket").click();//替代人手进行点击查询
+function checkTicket(arr) {
+  this.count = 0;//计数
+  let flag, num, ticketName;
+  this.timer = setInterval(() => {
+    this.count++;
+    arr.map((item) => {
+      flag = $('#' + item).attr("class")
+      if (flag) {
+        clearInterval(this.timer);
+        num = item.slice(-12)
+        ticketName = $(`#ticket_${num}`).find('a').html()
+        console.log("共查询：" + this.count + "次！");
+        var a = $(`<a href='${ticketName}:有票，快买' target='_blank'>查询结果</a>`).get(0);
+        a.click()
+      }
+    })
+    $("#query_ticket").click();
   }, 3000)
 }
-checkTicket();
+// checkTicket(['SWZ_6i0000G84000', 'SWZ_6i0000G8260B'])
