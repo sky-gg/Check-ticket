debf07daef7de2a08ab3edfd9ff15cd7631b5d38
sky-gg <33249562+sky-gg@users.noreply.github.com>
2019-03-08 18:41:59 +0800

    新增html5 桌面通知api

--- checkTicket.js
+++ checkTicket.js
@@ -1,22 +1,48 @@
+// 调用桌面通知
+if (!('Notification' in window)) {
+    alert('抱歉，您的浏览器不支持桌面通知，将终止查票');
+} else {
+    Notification.requestPermission().then(function (permission) {
+        if (permission === 'granted') {
+            // '用户允许通知' 继续执行
+            checkTicket(['SWZ_6i0000G84000', 'SWZ_6i0000G8260B'])
+        } else if (permission === 'denied') {
+            // '用户拒绝通知' 罢工
+            alert('抱歉，您拒绝使用桌面通知，将终止查票');
+        }
+    });
+}
 /*
 * param {arr} Array
-*/ 
+*/
 function checkTicket(arr) {
-  this.count = 0;//用于计数
-  this.timer = setInterval(() => {//设置定时器3秒查询一次
-    this.count++;
-    arr.map((item) => {//遍历传入数组
-      this.flag = $('#' + item).attr("class")//是否有class属性
-      if (this.flag) {//如果有证明有票
-        clearInterval(this.timer);//清除定时器，停止查询
-        this.num = item.slice(-12)//获取id后缀数字，用于查询有票的那个车次
-        this.ticketName = $(`#ticket_${this.num}`).find('a').html()//获取车次名称
-        console.log("共查询：" + this.count + "次！");
-        this.a = $(`<a href='${this.ticketName}:有票，快买' target='_blank'>查询结果</a>`).get(0);//使用a标签来最大化浏览器达到提醒功能
-        this.a.click()//打开新窗口
-      }
-    })
-    $("#query_ticket").click();//代替人手，进行查票操作
-  }, 3000)
+    //用于计数
+    this.count = 0;
+    //设置定时器3秒查询一次
+    this.timer = setInterval(() => {
+        this.count++;
+        //遍历传入数组
+        arr.map((item) => {
+            //是否有class属性
+            let flag = $('#' + item).attr("class")
+            //如果有证明有票
+            if (flag) {
+                //清除定时器，停止查询
+                clearInterval(this.timer);
+                //获取id后缀数字，用于查询有票的那个车次
+                let num = item.slice(-12)
+                //获取车次名称
+                let ticketName = $(`#ticket_${num}`).find('a').html()
+                // 调用api，进行电脑端提醒
+                new Notification(`已查询${this.count}次`, {
+                    dir: 'auto',
+                    body: `${ticketName}:车次有票，快买。`,
+                    icon: 'https://www.12306.cn/index/images/favicon.ico',
+                    requireInteraction: true
+                })
+            }
+        })
+        //代替人手，进行查票操作
+        $("#query_ticket").click();
+    }, 3000)
 }
-// checkTicket(['SWZ_6i0000G84000', 'SWZ_6i0000G8260B'])
